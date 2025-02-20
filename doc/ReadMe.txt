2f9ab9984e715f66a7c25153c0a17cd6809f24ef
Tiejun Ma <mat@chesterfield.gov>
2025-02-20 09:05:48 -0500

    Adding __init__.py

---
+++ doc/ReadMe.txt
@@ -0,0 +1,3 @@
+This folder is used to store the documents of this project.
+It includes:
+Structure.txt
---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+# About __init__.py 
+# __init__.py is a special file used to mark a directory as a Python package.
+# It can be empty, or it can include initialization code that runs when the package is imported.
+
+print("Importing Constellation Package")
